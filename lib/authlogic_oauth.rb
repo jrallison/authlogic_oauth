f4ee8298b786358d9d5f6ee79c2bf464336a6701
jrallison <jrallison@gmail.com>
2009-06-04 14:43:00 -0400

    Adding a few helpers for the register and login buttons

--- lib/authlogic_oauth.rb
+++ lib/authlogic_oauth.rb
@@ -2,6 +2,8 @@ require File.dirname(__FILE__) + "/authlogic_oauth/version"
 require File.dirname(__FILE__) + "/authlogic_oauth/oauth_process"
 require File.dirname(__FILE__) + "/authlogic_oauth/acts_as_authentic"
 require File.dirname(__FILE__) + "/authlogic_oauth/session"
+require File.dirname(__FILE__) + "/authlogic_oauth/helper"
 
 ActiveRecord::Base.send(:include, AuthlogicOauth::ActsAsAuthentic)
 Authlogic::Session::Base.send(:include, AuthlogicOauth::Session)
+ActionController::Base.helper AuthlogicOauth::Helper
---
+++ lib/authlogic_oauth/helper.rb
@@ -0,0 +1,16 @@
+module AuthlogicOauth
+  module Helper
+    def oauth_register_button(options = {})
+      oauth_button('register_with_oauth', options)
+    end
+    
+    def oauth_login_button(options = {})
+      oauth_button('login_with_oauth', options)
+    end
+  
+  private
+    def oauth_button(name, options = {})
+      '<input type="submit" value="#{options[:value]}" name="#{name}" id="user_submit" class="#{options[:class]}"/>'
+    end
+  end
+end
